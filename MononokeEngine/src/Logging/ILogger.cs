3a468dccc58ae7c88a2334e536b3fdc8493b94bc
Paco Juan <pacojuanquiros@gmail.com>
2019-07-15 18:14:52 +0200

    Open and close Tcp channel

--- MononokeEngine/src/Logging/ILogger.cs
+++ MononokeEngine/src/Logging/ILogger.cs
@@ -2,6 +2,12 @@ namespace MononokeEngine.Logging
 {
     public interface ILogger
     {
+
+        void Open();
+
+        void Close();
+        
+        
         void Print(string msg);
     }
 }
--- MononokeEngine/src/Logging/LoggerServer/ConsoleLoggerServer.cs
+++ MononokeEngine/src/Logging/LoggerServer/ConsoleLoggerServer.cs
@@ -11,28 +11,37 @@ namespace MononokeEngine.Logging
     public class ConsoleLoggerServer : ILogger
     {
 
-        public static readonly int Port = 9090;
+        public static readonly int Port = 8191;
+        
 #if DEBUG
+
+        private TcpChannel _serverChannel;
+
+        private bool _open;
+        
         private LoggerProxy _proxy;
         
         internal ConsoleLoggerServer()
         {
-            InitChannel();
+            _serverChannel = new TcpChannel(Port);
         }
         
+        
         [SecurityPermission(SecurityAction.Demand)]
-        private void InitChannel()
+        public void Open()
         {
-            // Create the server channel.
-            TcpChannel serverChannel = new TcpChannel(Port);
-            ChannelServices.RegisterChannel(serverChannel, true);
-
+            if (_open)
+                return;
+            
+            ChannelServices.RegisterChannel(_serverChannel, true);
+            _open = true;
+            
             // Show info
-            Console.WriteLine("The name of the channel is {0}.", serverChannel.ChannelName);
-            Console.WriteLine("The priority of the channel is {0}.", serverChannel.ChannelPriority);
+            Console.WriteLine("The name of the channel is {0}.", _serverChannel.ChannelName);
+            Console.WriteLine("The priority of the channel is {0}.", _serverChannel.ChannelPriority);
 
             // Show the URIs associated with the channel.
-            ChannelDataStore data = (ChannelDataStore) serverChannel.ChannelData;
+            ChannelDataStore data = (ChannelDataStore) _serverChannel.ChannelData;
             foreach (string uri in data.ChannelUris)
             {
                 Console.WriteLine("The channel URI is {0}.", uri);
@@ -47,12 +56,12 @@ namespace MononokeEngine.Logging
             _proxy = (LoggerProxy) Activator.GetObject(typeof(LoggerProxy), "tcp://localhost:9090/LoggerProxy.rem");
 
             // Parse the channel's URI.
-            string[] urls = serverChannel.GetUrlsForUri("LoggerProxy.rem");
+            string[] urls = _serverChannel.GetUrlsForUri("LoggerProxy.rem");
             if (urls.Length > 0)
             {
                 string objectUrl = urls[0];
                 string objectUri;
-                string channelUri = serverChannel.Parse(objectUrl, out objectUri);
+                string channelUri = _serverChannel.Parse(objectUrl, out objectUri);
                 Console.WriteLine("The object URL is {0}.", objectUrl);
                 Console.WriteLine("The object URI is {0}.", objectUri);
                 Console.WriteLine("The channel URI is {0}.", channelUri);
@@ -60,6 +69,19 @@ namespace MononokeEngine.Logging
 
         }
 #endif
+
+
+        public void Close()
+        {
+            if (!_open)
+                return;
+            
+            ChannelServices.UnregisterChannel(_serverChannel);
+            _open = false;
+        }
+        
+        
+        
         public void Print(string msg)
         {
 #if DEBUG
