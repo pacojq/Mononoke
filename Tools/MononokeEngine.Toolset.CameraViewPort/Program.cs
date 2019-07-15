ac12f23abd325d77bc136c4d9a3e84f615c60998
Paco Juan <pacojuanquiros@gmail.com>
2019-07-15 18:46:56 +0200

    Created toolset project

---
+++ Tools/MononokeEngine.Toolset.CameraViewPort/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MononokeEngine.Toolset.CameraViewPort
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // TODO
+        }
+    }
+}
